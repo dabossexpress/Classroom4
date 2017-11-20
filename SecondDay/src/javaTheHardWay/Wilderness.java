8fc5db0888564bb3261a34814c9325fddc54f416
BT_1N3_20 <BT_1N3_20@BT_1N3_20.bktech.local>
2017-11-20 10:33:21 -0500

    created 3 new classes

---
+++ SecondDay/src/javaTheHardWay/Wilderness.java
@@ -0,0 +1,47 @@
+package javaTheHardWay;
+
+public class Wilderness extends Habitat {
+	
+	private String description;
+	
+	public Wilderness(String description, int livingCapacity) {
+		// TODO Auto-generated constructor stub
+		super(livingCapacity);
+		this.description = description;
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Wilderness w = new Wilderness("An empty desert",10);
+		w.simulate(3);
+	}
+	
+	public String toString() {
+		return this.description +"\n"+ animalReport();
+	}
+
+	public void simulate(int rounds) {
+		for(int i = 0;i<rounds;i++) {
+			for(Animal a: getAnimals()) {
+				if(a instanceof ReproductionAnimal)((ReproductionAnimal)a).reset();
+			}
+			int predatorCount = 0;
+			int preyCount = 0;
+			for(Animal a: getAnimals()) {
+				//Note: you must create a Predator and Prey class, but they can be empty
+				if(a instanceof Predator) {
+					predatorCount++;
+				}
+				else if(a instanceof Prey) {
+					preyCount++;
+				}
+			}
+			String s = "\nROUND" + (i+1)+"\nPREDATORS: " +predatorCount+", PREY: "+preyCount+"\n"+animalReport();
+			System.out.println(s);
+			for(Animal a: getAnimals()) {
+				if(a != null)a.act();
+			}
+			growPlants();
+		}
+	}
+}
